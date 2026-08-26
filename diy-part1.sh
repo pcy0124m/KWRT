7c98c52af11efc11c3bc3d633f56bc50ef4b9110
pcy0124m <pcy0124m@users.noreply.github.com>
2026-08-26 16:04:04 +0800

    fix: 开启 PassWall 源，解决编译依赖缺失

--- diy-part1.sh
+++ diy-part1.sh
@@ -7,8 +7,9 @@ echo 'src-git argon https://github.com/jerrykuku/luci-theme-argon.git' >> feeds.
 # ---------- 添加 Turbo ACC 网络加速源 ----------
 echo 'src-git turboacc https://github.com/chenmozhijin/turboacc.git' >> feeds.conf.default
 
-# ---------- （可选）PassWall 源 ----------
-# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' >> feeds.conf.default
+# ---------- PassWall 源（LEDE 默认依赖，必须开启） ----------
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' >> feeds.conf.default
+echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git' >> feeds.conf.default
 
 # ---------- （可选）OpenClash 源 ----------
 # echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >> feeds.conf.default
