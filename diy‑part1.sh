6907c941ede83b9fedea5094c1e2f2f6c17c7e76
pcy0124m <765266885@qq.com>
2026-08-26 14:45:30 +0800

    Update diy‑part1.sh

--- diy‑part1.sh
+++ diy‑part1.sh
@@ -2,19 +2,13 @@
 echo "Kwrt feeds init done"
 # ---------- 添加 Argon 主题源 ----------
 echo 'src-git argon https://github.com/jerrykuku/luci-theme-argon.git' >> feeds.conf.default
-
 # ---------- 添加 Turbo ACC 网络加速源 ----------
 echo 'src-git turboacc https://github.com/chenmozhijin/turboacc.git' >> feeds.conf.default
-
 # ---------- （可选）添加 PassWall 源，需要的话取消注释 ----------
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' >> feeds.conf.default
 # echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git' >> feeds.conf.default
 # echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git' >> feeds.conf.default
-
 # ---------- （可选）添加 OpenClash 源，需要的话取消注释 ----------
 # echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >> feeds.conf.default
-
-# ---------- （可选）克隆单个插件到 package 目录 ----------
-# git clone https://github.com/xxx/luci-app-xxx.git package/luci-app-xxx
 
 echo "===== diy-part1.sh 执行完成 ====="
